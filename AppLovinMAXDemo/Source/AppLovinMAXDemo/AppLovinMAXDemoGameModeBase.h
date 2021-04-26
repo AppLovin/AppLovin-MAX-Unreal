e960c826a86a7348fa983b12b96e17cf2b6b260f
Ritam Sarmah <ritam.sarmah@applovin.com>
2021-04-26 11:45:33 -0600

    Initial commit

---
+++ AppLovinMAXDemo/Source/AppLovinMAXDemo/AppLovinMAXDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AppLovinMAXDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPLOVINMAXDEMO_API AAppLovinMAXDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
