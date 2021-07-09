e370e27823ceb9ebbc375da1eaa691bde9ba6a71
Ritam Sarmah <ritam.sarmah@applovin.com>
2021-07-09 11:31:52 -0700

    Code cleanup

--- AppLovinMAXDemo/Plugins/AppLovinMAX/Source/ThirdParty/IOS/Adapters/install_adapters.py
+++ AppLovinMAXDemo/Plugins/AppLovinMAX/Source/ThirdParty/IOS/Adapters/install_adapters.py
@@ -13,13 +13,11 @@ from urllib.request import urlretrieve
 from pathlib import Path
 from zipfile import ZipFile
 
-adapter_code = []
+build_rules = []
 
 
 def parse_podfile():
     with open("Podfile") as podfile:
-        print("> Installing adapters")
-
         # Trim whitespace
         pods = map(lambda l: l.strip(), podfile.readlines())
 
@@ -29,19 +27,17 @@ def parse_podfile():
         # Get pod names
         pods = map(lambda l: l.split(' ', 1)[1].strip("\'"), pods)
 
+        # Only get AppLovin adapters
+        pods = filter(lambda l: l.startswith("AppLovinMediation"), pods)
+
         return pods
 
 
-# Parse and retrieve top-level pod dependencies
-def parse_pods(pods):
+def install_pods(pods):
     for pod in pods:
-        if not pod.startswith("AppLovinMediation"):
-            continue
-
         process = subprocess.run(
             ["pod", "spec", "cat", pod], capture_output=True)
         spec = json.loads(process.stdout.decode().strip())
-
         create_embedded_framework(spec)
 
 
@@ -50,32 +46,32 @@ def create_embedded_framework(spec):
     url = spec["source"]["http"]
     version = spec["version"]
 
-    print(f"Installing {name} ({version})")
+    print(f"{name} ({version})")
 
     # Download and save adapter
-    zip_path = Path(f"../{name}.zip")
+    zip_path = Path(f"{name}.zip")
     urlretrieve(url, zip_path)
 
     # Extract into IOS directory
-    extract_path = Path(f"{name}-{version}")
     with ZipFile(zip_path, 'r') as zip_file:
         zip_file.extractall()
 
     # Delete zip file
     zip_path.unlink()
 
-    # Rename
+    # Rename extracted directory and move to parent directory
+    extract_path = Path(f"{name}-{version}")
     embedded_path = Path(f"../{name}.embeddedframework")
     embedded_zip_path = Path(f"../{name}.embeddedframework.zip")
     extract_path.rename(embedded_path)
 
     # Create embeddedframework.zip
     # NOTE: shutil.make_archive creates a ZIP file that doesn't work with Unreal for some reason
-    subprocess.call(["zip", "-rq", embedded_zip_path, embedded_path])
+    subprocess.run(["zip", "-rq", embedded_zip_path, embedded_path])
     shutil.rmtree(embedded_path)
 
     # Generate build settings
-    code = f"""
+    rule = f"""
 PublicAdditionalFrameworks.Add(
     new Framework(
         "{name}",
@@ -83,22 +79,23 @@ PublicAdditionalFrameworks.Add(
     )
 );"""
 
-    adapter_code.append(code)
+    build_rules.append(rule)
 
 
 def main():
-    print("NOTE: This script only installs AppLovin adapters and does not retrieve the third-party SDKs. Please refer to the README to complete your integration.\n")
+    print("This script only installs AppLovin adapters and does not retrieve the third-party SDKs.\nPlease refer to the README instructions to complete your integration.\n")
 
     print("> Updating CocoaPods repos... (this may take a while)")
-    subprocess.call(["pod", "--silent", "repo", "update"])
+    subprocess.run(["pod", "--silent", "repo", "update"])
 
+    print("> Installing adapters...")
     pods = parse_podfile()
-    parse_pods(pods)
+    install_pods(pods)
 
-    print(f"\n> Installed {len(adapter_code)} adapter(s)")
+    print(f"\n> Installed {len(build_rules)} adapter(s)")
     print("> Copy the iOS build rules below into AppLovinMAX.Build.cs. You will also need to add build rules for the third-party SDKs later.")
-    print("\n".join(adapter_code))
+    print("\n".join(build_rules))
+
 
 if __name__ == "__main__":
     main()
-
