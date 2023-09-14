931d1a581cf6bfc7aef197b6b0173f9f45a1a740
Ritam Sarmah <ritam.sarmah@applovin.com>
2023-09-13 17:12:15 -0700

    Add target build settings for 5.2 and 5.3

--- AppLovinMAXDemo/Source/AppLovinMAXDemo.Target.cs
+++ AppLovinMAXDemo/Source/AppLovinMAXDemo.Target.cs
@@ -8,14 +8,24 @@ public class AppLovinMAXDemoTarget : TargetRules
 	public AppLovinMAXDemoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+
+#if UE_5_3_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+#elif UE_5_2_OR_LATER
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+#else
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
+
 		ExtraModuleNames.AddRange( new string[] { "AppLovinMAXDemo" } );
 
-		if ( Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac )
-		{
-			// Add compiler argument to support building on macOS with Xcode 13.3+
-			bOverrideBuildEnvironment = true;
-			AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-bitwise-instead-of-logical -Wno-single-bit-bitfield-constant-conversion";
-		}		
+		// Add compiler arguments to support building on macOS with Xcode 13.3+
+		// if ( Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac )
+		// {
+			// bOverrideBuildEnvironment = true;
+			// AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-bitwise-instead-of-logical -Wno-single-bit-bitfield-constant-conversion";
+		// }		
 	}
 }
--- AppLovinMAXDemo/Source/AppLovinMAXDemoEditor.Target.cs
+++ AppLovinMAXDemo/Source/AppLovinMAXDemoEditor.Target.cs
@@ -8,13 +8,24 @@ public class AppLovinMAXDemoEditorTarget : TargetRules
 	public AppLovinMAXDemoEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+	
+#if UE_5_3_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+#elif UE_5_2_OR_LATER
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+#else
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
+
 		ExtraModuleNames.AddRange( new string[] { "AppLovinMAXDemo" } );
 
-		if ( Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac )
-		{
-			bOverrideBuildEnvironment = true;
-			AdditionalCompilerArguments = "-Wno-unknown-warning-option -Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-bitwise-instead-of-logical -Wno-single-bit-bitfield-constant-conversion";
-		}		
+		// Add compiler arguments to support building on macOS with Xcode 13.3+
+		// if ( Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac )
+		// {
+			// bOverrideBuildEnvironment = true;
+			// AdditionalCompilerArguments = "-Wno-unknown-warning-option -Wno-unused-but-set-variable -Wno-deprecated-builtins -Wno-bitwise-instead-of-logical -Wno-single-bit-bitfield-constant-conversion";
+		// }		
 	}
 }
