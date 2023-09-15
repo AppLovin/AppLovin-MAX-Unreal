57627cfb4574a93f93ebfd12bc3fb6a74102339c
Ritam Sarmah <ritam.sarmah@applovin.com>
2023-09-15 10:49:26 -0700

    Clean up settings

--- AppLovinMAXDemo_5.2/Source/AppLovinMAXDemo/Public/Constants.h
+++ AppLovinMAXDemo_5.2/Source/AppLovinMAXDemo/Public/Constants.h
@@ -3,20 +3,20 @@
 
 #pragma once
 
-const FString SdkKey = "hBDh6tzZrp-fWye63N4nhbgw8umnTzD99QsGIpq8bpo7lRDppHZVuEQ17Bpa80lIRaTlONt-Af6v5JiubGOUVp";
+const FString SdkKey = "YOUR_SDK_KEY";
 
 namespace AdUnitIdentifier
 {
 #if PLATFORM_IOS
-    const FString Banner = "35858295296a8c80";
-    const FString MRec = "f865a23962fa00e5";
-    const FString Interstitial = "65d8d0195e50bda6";
-    const FString Rewarded = "82076aefed4737ed";
+    const FString Banner = "IOS_BANNER_AD_UNIT_ID";
+    const FString MRec = "IOS_MREC_AD_UNIT_ID";
+    const FString Interstitial = "IOS_INTERSTITIAL_AD_UNIT_ID";
+    const FString Rewarded = "IOS_REWARDED_AD_UNIT_ID";
 #elif PLATFORM_ANDROID
-    const FString Banner = "3c2e14fa5f22c4f6";
-    const FString MRec = "6e882d2f921d6d7c";
-    const FString Interstitial = "f5df6799ac75cd34";
-    const FString Rewarded = "49cdb15b16042a49";
+    const FString Banner = "ANDROID_BANNER_AD_UNIT_ID";
+    const FString MRec = "ANDROID_MREC_AD_UNIT_ID";
+    const FString Interstitial = "ANDROID_INTERSTITIAL_AD_UNIT_ID";
+    const FString Rewarded = "ANDROID_REWARDED_AD_UNIT_ID";
 #else // Empty values for compiling in editor or other platforms
     const FString Banner = "";
     const FString MRec = "";
